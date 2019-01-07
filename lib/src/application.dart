30e6f5b0159cc9404aef861a1d56691f5eede1f4
JohanBaskovec <johan@johanbaskovec.fr>
2019-01-07 03:05:14 +0100

    Load fixtures at start

--- lib/src/application.dart
+++ lib/src/application.dart
@@ -6,11 +6,40 @@ import 'package:blog_backend/src/postgres_connection_factory.dart';
 import 'package:blog_backend/src/routing/router.dart';
 import 'package:blog_backend/src/routing_context.dart';
 import 'package:blog_backend/src/typing/book_controller.dart';
+import 'package:blog_backend/src/typing/text_repository.dart';
 import 'package:blog_backend/src/typing/typing_text_controller.dart';
 import 'package:blog_backend/src/utf8_stream_to_json_converter.dart';
 import 'package:blog_backend/src/utf8_stream_to_object_converter.dart';
 import 'package:blog_backend/src/utf8_stream_to_string_converter.dart';
+import 'package:blog_common/blog_common.dart';
+import 'package:postgres/postgres.dart';
 import 'package:yaml/yaml.dart';
+
+/// Load test data
+Future<void> loadFixtures(
+    PostgresConnectionFactory postgresConnectionFactory) async {
+  final PostgreSQLConnection connection =
+      await postgresConnectionFactory.newOpenConnection();
+
+  final textRepository = TextRepository(connection);
+  await textRepository.persist(Text('title 1', '''
+Mr. Bennet was so odd a mixture of quick parts, sarcastic humour,
+reserve, and caprice, that the experience of three-and-twenty years had
+been insufficient to make his wife understand his character. _Her_ mind
+was less difficult to develop. She was a woman of mean understanding,
+little information, and uncertain temper. When she was discontented,
+she fancied herself nervous. The business of her life was to get her
+daughters married; its solace was visiting and news.'''));
+
+  await textRepository.persist(Text('title 2', '''
+In a few days Mr. Bingley returned Mr. Bennet's visit, and sat about
+ten minutes with him in his library. He had entertained hopes of being
+admitted to a sight of the young ladies, of whose beauty he had
+heard much; but he saw only the father. The ladies were somewhat more
+fortunate, for they had the advantage of ascertaining from an upper
+window that he wore a blue coat, and rode a black horse.'''));
+  await connection.close();
+}
 
 /// Run the application.
 Future<void> run() async {
@@ -31,6 +60,9 @@ Future<void> run() async {
         config['db_name'] as String,
         config['db_username'] as String,
         config['db_password'] as String);
+    if (config['load_fixtures'] as bool) {
+      await loadFixtures(postgresConnectionFactory);
+    }
     final HttpServer server =
         await HttpServer.bind(InternetAddress.anyIPv6, backendPort);
     print('Listening on http://localhost:$backendPort.');
